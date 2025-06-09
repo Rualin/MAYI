583f54035134685a463496efc12269e4d760febc
VishA1308 <a.vishnyakova1@g.nsu.ru>
2025-06-09 16:24:39 +0700

    Update Information From Name.py

--- Information From Name.py
+++ Information From Name.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def fetch_dishes_by_name(dish_names):
     try:
-        # Установка соединения с базой данных
+        # Установка соединения с базой данных PostgreSQL
         conn = psycopg2.connect(
             host="localhost",
             database="upp",
@@ -11,7 +11,7 @@ def fetch_dishes_by_name(dish_names):
         )
         cursor = conn.cursor()
 
-        # Запрос на получение блюд и их ингредиентов
+        # Запрос для получения блюд по их названиям и их ингредиентов
         query = f"""
         SELECT d.id, d.name, d.receipt, d.category1_id, d.category2_id, d.time, 
                array_agg(i.name) AS ingredients
@@ -19,30 +19,32 @@ def fetch_dishes_by_name(dish_names):
         LEFT JOIN dish_ingredients di ON d.id = di.dish_id
         LEFT JOIN ingredients i ON di.ingredient_id = i.id
         WHERE d.name IN ({','.join(['%s'] * len(dish_names))})
-        GROUP BY d.id;
+        GROUP BY d.id;  # Группировка результатов по ID блюда
         """
 
+        # Выполнение запроса с параметрами dish_names
         cursor.execute(query, dish_names)
-        results = cursor.fetchall()
+        results = cursor.fetchall()  # Получение всех результатов запроса
 
         if results:
+            # Вывод заголовков таблицы
             print(f"{'ID':<5} {'Название':<30} {'Рецепт':<50} {'Категория 1 ID':<15} {'Категория 2 ID':<15} {'Время приготовления':<20} {'Ингредиенты':<50}")
             print("=" * 120)
             for row in results:
+                # Форматированный вывод данных о каждом блюде
                 print(f"{'ID':} {row[0]:}\n\n{'Название':} {row[1]:}\n\n{'Рецепт':} {row[2]:}\n\n{'Категория 1 ID':} {row[3]:}\n\n{'Категория 2 ID':} {row[4]:}\n\n{'Время приготовления':} {row[5]:}\n\n{'Ингредиенты':} {', '.join(row[6]) if row[6] else 'Нет ингредиентов':}\n\n")
         else:
-            print("Нет блюд с указанными именами.")
+            print("Нет блюд с указанными именами.")  
 
     except psycopg2.Error as e:
+        # Обработка ошибок при работе с базой данных
         print("Ошибка при работе с базой данных:", e)
     finally:
-        
+        # Закрытие курсора и соединения с базой данных
         if cursor:
             cursor.close()
         if conn:
             conn.close()
 
-
-
 dish_names_list = ['Омлет', 'Глазунья'] 
-fetch_dishes_by_name(dish_names_list)
+fetch_dishes_by_name(dish_names_list)  # Вызов функции для получения информации о блюдах
