c512aac08f3632b4f20be1af1891a8a404035301
Marina Zhiltsova <m.zhiltsova@g.nsu.ru>
2025-06-09 16:31:57 +0700

    Update db_connection.py

--- backend/src/utils/db_connection.py
+++ backend/src/utils/db_connection.py
@@ -1,5 +1,8 @@
 import psycopg2
 from config.config import db_config
 
+# Подключение к базе данных через db_config
+# Файла config.py нет на гите, так как нужно вручную создавать базу данных,
+# а потом в db_config записывать логин, пароль и т.д.
 def get_db_connection():
     return psycopg2.connect(**db_config)
